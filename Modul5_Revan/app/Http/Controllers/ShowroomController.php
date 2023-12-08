804735e20cf9a70a25535feeec7598876b3f8b89
revandaoctavian <octavianrevan@gmail.com>
2023-12-08 15:36:15 +0700

    artur cina

---
+++ Modul5_Revan/app/Http/Controllers/ShowroomController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Showroom;
+
+class ShowroomController extends Controller
+{
+    public function create()
+    {
+        return view('showroom.create');
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        Showroom::create([
+            'nama_mobil' => $data['name'],
+            'brand_mobil' => $data['brand'],
+            'warna_mobil' => $data['warna'],
+            'tipe_mobil' => $data['tipe'],
+            'harga_mobil' => $data['harga'],
+
+        ]);
+        return redirect()->route('showroom.index');
+    }
+
+    public function index()
+    {
+
+        return view('showroom.index', [
+            'showroom' => Showroom::all()
+        ]);
+    }
+}
